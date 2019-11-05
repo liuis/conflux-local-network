c134ae1210d96238114a24e9331ca8caedb0b2bd
liuis <ping.li@conflux.org>
2019-11-05 17:04:24 +0800

    fixed bugs

--- start.sh
+++ start.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-pip install  ecdsa
-pip install  pysha3 >= 1.0.2
-
-ehco "genesis_accounts="genesis.toml" >> ./run/default.toml
 
 ./target/release/conflux --config ./run/conflux.conf --public-address 127.0.0.1:14629 
